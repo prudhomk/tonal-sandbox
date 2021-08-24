6c8d5016d8cbb03e55d548c6703f9be47a98ebf8
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-24 14:59:26 -0700

    RefactorChords - added name key & value

--- src/components/data/refactorChords.js
+++ src/components/data/refactorChords.js
@@ -1,114 +1,133 @@
 /* eslint-disable max-len */
 export const C = {
-  'C': ['Db', 'D', 'Dm', 'Eb', 'E', 'Em', 'F', 'Fm', 'Gb', 'G', 'Gm', 'Ab', 'A', 'Am', 'Abm', 'Bb', 'Bm', 'B'],
+  'name': 'C',
+  'chords': ['Db', 'D', 'Dm', 'Eb', 'E', 'Em', 'F', 'Fm', 'Gb', 'G', 'Gm', 'Ab', 'A', 'Am', 'Abm', 'Bb', 'Bm', 'B'],
   'colors': [],
   'tones': [],
 };
 
 export const Db = {
-  'Db': ['C', 'Bb', 'Ab'],
+  'name': 'Db',
+  'chords': ['C', 'Bb', 'Ab'],
   'colors': [],
   'tones': [],
 };
 
 export const Dm = {
-  'Dm': ['C', 'G', 'Gm', 'Em', 'Am', 'Ab', 'Fm', 'F', 'Eb', 'Db'],
+  'name': 'Dm',
+  'chords': ['C', 'G', 'Gm', 'Em', 'Am', 'Ab', 'Fm', 'F', 'Eb', 'Db'],
   'colors': [],
   'tones': [],
 };
 
 export const D = {
-  'D': ['C', 'Db', 'G', 'F', 'Fm', 'Am', 'Ab', 'Bb'],
+  'name': 'D',
+  'chords': ['C', 'Db', 'G', 'F', 'Fm', 'Am', 'Ab', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Eb = {
-  'Eb': ['C', 'Db', 'D', 'Dm', 'F', 'Fm', 'Ab', 'Bb'],
+  'name': 'Eb',
+  'chords': ['C', 'Db', 'D', 'Dm', 'F', 'Fm', 'Ab', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Em = {
-  'Em': ['C', 'F', 'Fm', 'Am', 'E', 'Eb', 'G', 'Dm', 'D', 'Db', 'Bb', 'Bm'],
+  'name': 'Em',
+  'chords': ['C', 'F', 'Fm', 'Am', 'E', 'Eb', 'G', 'Dm', 'D', 'Db', 'Bb', 'Bm'],
   'colors': [],
   'tones': [],
 };
 
 export const E = {
-  'E': ['C', 'Bb', 'Am', 'Em', 'F', 'Fm', 'G'],
+  'name': 'E',
+  'chords': ['C', 'Bb', 'Am', 'Em', 'F', 'Fm', 'G'],
   'colors': [],
   'tones': [],
 };
 
 export const F = {
-  'F': ['C', 'G', 'Am', 'Bb', 'Fm', 'Ab', 'Db', 'Dm', 'D', 'Em', 'E', 'Eb'],
+  'name': 'F',
+  'chords': ['C', 'G', 'Am', 'Bb', 'Fm', 'Ab', 'Db', 'Dm', 'D', 'Em', 'E', 'Eb'],
   'colors': [],
   'tones': [],
 };
 
 export const Fm = {
-  'Fm': ['C', 'Db', 'Eb', 'E', 'G', 'Ab', 'Am', 'Bb'],
+  'name': 'Fm',
+  'chords': ['C', 'Db', 'Eb', 'E', 'G', 'Ab', 'Am', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Gb = {
-  'Gb': ['C', 'F', 'Bb', 'B'],
+  'name': 'Gb',
+  'chords': ['C', 'F', 'Bb', 'B'],
   'colors': [],
   'tones': [],
 };
 
 export const G = {
-  'G': ['C', 'Fm', 'F', 'Am', 'Ab', 'Eb', 'Db', 'Dm', 'Bb'],
+  'name': 'G',
+  'chords': ['C', 'Fm', 'F', 'Am', 'Ab', 'Eb', 'Db', 'Dm', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Gm = {
-  'Gm': ['C'],
+  'name': 'Gm',
+  'chords': ['C'],
   'colors': [],
   'tones': [],
 };
 
 export const Ab = {
-  'Ab': ['C', 'Bb', 'G', 'Db', 'Am', 'F', 'Fm', 'Eb', 'Dm', 'D', 'E'],
+  'name': 'Ab',
+  'chords': ['C', 'Bb', 'G', 'Db', 'Am', 'F', 'Fm', 'Eb', 'Dm', 'D', 'E'],
   'colors': [],
   'tones': [],
 };
 
 export const Abm = {
-  'Abm': ['C', 'Db', 'F', 'Fm', 'G', 'Bb'],
+  'name': 'Abm',
+  'chords': ['C', 'Db', 'F', 'Fm', 'G', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const A = {
-  'A': ['C', 'D', 'Ab', 'F', 'Fm', 'Bb'],
+  'name': 'A',
+  'chords': ['C', 'D', 'Ab', 'F', 'Fm', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Am = {
-  'Am': ['C', 'Db', 'Dm', 'Eb', 'Em', 'E', 'F', 'Fm', 'G', 'Ab', 'Bb'],
+  'name': 'Am',
+  'chords': ['C', 'Db', 'Dm', 'Eb', 'Em', 'E', 'F', 'Fm', 'G', 'Ab', 'Bb'],
   'colors': [],
   'tones': [],
 };
 
 export const Bb = {
-  'Bb': ['C', 'Db', 'Eb', 'B', 'Ab', 'F', 'Fm', 'G', 'D', 'A', 'Am', 'E'],
+  'name': 'Bb',
+  'chords': ['C', 'Db', 'Eb', 'B', 'Ab', 'F', 'Fm', 'G', 'D', 'A', 'Am', 'E'],
   'colors': [],
   'tones': [],
 };
 
 export const B = {
-  'B': ['C', 'Bb', 'Em', 'Bm', 'E', 'Am', 'G', 'D', 'Dm'],
+  'name': 'B',
+  'chords': ['C', 'Bb', 'Em', 'Bm', 'E', 'Am', 'G', 'D', 'Dm'],
   'colors': [],
   'tones': [],
 };
 
 export const Bm = {
-  'Bm': ['C'],
+  'name': 'Bm',
+  'chords': ['C'],
   'colors': [],
   'tones': [],
 };
